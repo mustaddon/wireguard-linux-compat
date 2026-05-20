2c9ba012b1551fea5b7decc0b3f2273171ec0400
mustaddon <mustaddon@gmail.com>
2026-05-21 00:33:16 +0200

    t4

--- src/hidden.c
+++ src/hidden.c
@@ -2,20 +2,12 @@
 #include "messages.h"
 #include "peerlookup.h"
 
-static const unsigned char MASK[32] = { 
-    0x81, 0xab, 0xa4, 0x0d, 0xb7, 0x73, 0x42, 0x2b, 
-    0xd0, 0x79, 0x2d, 0x65, 0xce, 0x69, 0x1f, 0x82, 
-    0x98, 0x31, 0x89, 0xaf, 0xd6, 0x5c, 0x85, 0x93, 
-    0x8b, 0x90, 0x52, 0x33, 0x17, 0xff, 0x18, 0x57 };
 
 #define HIDDEN_HEADER_LEN(val) ((val)&7)
 #define SKB_HIDDEN_HEADER_LEN(skb) HIDDEN_HEADER_LEN(((u8 *)(skb))[0])
 #define HIDDEN_TYPE(val) ((val)&7)
 #define SKB_HIDDEN_TYPE(skb) HIDDEN_TYPE(((u8 *)(skb))[1])
-
-#define	U8_MASK(wg) (MASK)
-#define	U32_MASK(wg) (((u32 *)(MASK)))
-#define	XOR_HEAD(skb, wg) ((u8 *)(skb))[0]=(((u8 *)(skb))[0]&0xF0)|((((u8 *)(skb))[0]^U8_MASK(wg)[0])&0x0F)
+#define	XOR_HEAD(skb, mask) ((u8 *)(skb))[0]=(((u8 *)(skb))[0]&0xF0)|((((u8 *)(skb))[0]^(mask)[3])&0x0F)
 
 
 static void xor_mac2(void *skb, size_t len, u32 zero, u32 *mask)
@@ -27,18 +19,16 @@ static void xor_mac2(void *skb, size_t len, u32 zero, u32 *mask)
     ptr[3] ^= zero^mask[7];
 }
 
-static void xor_init(void *skb, struct wg_device *wg)
+static void xor_init(void *skb, u32 *mask)
 {
-    u32 *mask = U32_MASK(wg);
     u32 *ptr = (u32 *)(skb);
     u32 zero = ptr[0]^mask[0];
     ptr[1] ^= zero^mask[1];
     xor_mac2(skb, sizeof(struct message_handshake_initiation), zero, mask);
 }
 
-static void xor_resp(void *skb, struct wg_device *wg)
+static void xor_resp(void *skb, u32 *mask)
 {
-    u32 *mask = U32_MASK(wg);
     u32 *ptr = (u32 *)(skb);
     u32 zero = ptr[0]^mask[0];
     ptr[1] ^= zero^mask[1];
@@ -46,17 +36,15 @@ static void xor_resp(void *skb, struct wg_device *wg)
     xor_mac2(skb, sizeof(struct message_handshake_response), zero, mask);
 }
 
-static void xor_cook(void *skb, struct wg_device *wg)
+static void xor_cook(void *skb, u32 *mask)
 {
-    u32 *mask = U32_MASK(wg);
     u32 *ptr = (u32 *)(skb);
     u32 zero = ptr[0]^mask[0];
     ptr[1] ^= zero^mask[1];
 }
 
-static void xor_data(void *skb, struct wg_device *wg)
+static void xor_data(void *skb, u32 *mask)
 {
-    u32 *mask = U32_MASK(wg);
     u32 *ptr = (u32 *)(skb);
     u32 zero = ptr[0]^mask[0];
     ptr[1] ^= zero^mask[1];
@@ -72,7 +60,7 @@ size_t prepare_skb_hidden(struct sk_buff *skb, struct wg_device *wg)
     if (unlikely(!pskb_may_pull(skb, 32)))
         return ERROR_HIDDEN_LEN;
 
-    XOR_HEAD(skb->data, wg);
+    XOR_HEAD(skb->data, wg->static_identity.static_public);
     hlen = HIDDEN_HEADER_LEN(((u8 *)(skb->data))[0]);
 
     if (((u8 *)(skb->data))[0] & 0x80)
@@ -108,25 +96,25 @@ size_t prepare_skb_hidden(struct sk_buff *skb, struct wg_device *wg)
         case MESSAGE_DATA:
             if (unlikely(!pskb_may_pull(skb, sizeof(struct message_data))))
                 return ERROR_HIDDEN_LEN;
-            xor_data(skb->data, wg);
+            xor_data(skb->data, (u32 *)wg->static_identity.static_public);
             break;
 
         case MESSAGE_HANDSHAKE_INITIATION:
             if (unlikely(!pskb_may_pull(skb, sizeof(struct message_handshake_initiation))))
                 return ERROR_HIDDEN_LEN;
-            xor_init(skb->data, wg);
+            xor_init(skb->data, (u32 *)wg->static_identity.static_public);
             break;
 
         case MESSAGE_HANDSHAKE_RESPONSE:
             if (unlikely(!pskb_may_pull(skb, sizeof(struct message_handshake_response))))
                 return ERROR_HIDDEN_LEN;
-            xor_resp(skb->data, wg);
+            xor_resp(skb->data, (u32 *)wg->static_identity.static_public);
             break;
 
         case MESSAGE_HANDSHAKE_COOKIE:
             if (unlikely(!pskb_may_pull(skb, 8)))
                 return ERROR_HIDDEN_LEN;
-            xor_cook(skb->data, wg);
+            xor_cook(skb->data, (u32 *)wg->static_identity.static_public);
             break;
 
         default:
@@ -193,7 +181,7 @@ void skb_push_hidden_handshake(void *skb, void *buffer, struct wg_peer *peer)
             quic = (u8 *)skb_push(skb, qlen + hlen);
             add_quick_init((struct message_handshake_initiation *)buffer,
                 (struct QUIC_init *)(quic + sizeof(struct QUIC_init_start)));
-            xor_init(buffer, peer->device);
+            xor_init(buffer, (u32 *)peer->handshake.remote_static);
             break;
 
         case MESSAGE_HANDSHAKE_RESPONSE:
@@ -202,7 +190,7 @@ void skb_push_hidden_handshake(void *skb, void *buffer, struct wg_peer *peer)
             quic = (u8 *)skb_push(skb, qlen + hlen);
             add_quick_resp((struct message_handshake_response *)buffer,
                 (struct QUIC_resp *)(quic + sizeof(struct QUIC_init_start)));
-            xor_resp(buffer, peer->device);
+            xor_resp(buffer, (u32 *)peer->handshake.remote_static);
             break;
 
         case MESSAGE_HANDSHAKE_COOKIE:
@@ -211,7 +199,7 @@ void skb_push_hidden_handshake(void *skb, void *buffer, struct wg_peer *peer)
             quic = (u8 *)skb_push(skb, qlen + hlen);
             add_quick_cook((struct message_handshake_cookie *)buffer,
                 (struct QUIC_cook *)(quic + sizeof(struct QUIC_init_start)));
-            xor_cook(buffer, peer->device);
+            xor_cook(buffer, (u32 *)peer->handshake.remote_static);
             break;
     }
 
@@ -219,7 +207,7 @@ void skb_push_hidden_handshake(void *skb, void *buffer, struct wg_peer *peer)
     add_quick_init_end(dlen + hlen, (struct QUIC_init_end *)(quic + qlen - sizeof(struct QUIC_init_end)));
     if (hlen > 0) get_random_bytes(quic+qlen, hlen);
 
-    XOR_HEAD(quic, peer->device);
+    XOR_HEAD(quic, peer->handshake.remote_static);
 }
 
 void skb_push_hidden_handshake_cookie(void *skb, void *buffer, struct wg_device *wg)
@@ -260,6 +248,6 @@ void skb_push_hidden_data(void *skb, void *buffer, unsigned int hlen, struct wg_
         memcpy(&quic[1], &((struct message_data *)buffer)->key_idx, 3);
     }
 
-    xor_data(buffer, peer->device);
-    XOR_HEAD(quic, peer->device);
+    xor_data(buffer, (u32 *)peer->handshake.remote_static);
+    XOR_HEAD(quic, peer->handshake.remote_static);
 }
