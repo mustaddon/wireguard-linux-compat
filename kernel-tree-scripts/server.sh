40ee00489189b41343edbb6919604c05e009a9bc
mustaddon <mustaddon@gmail.com>
2026-05-21 14:20:39 +0200

    sh

--- kernel-tree-scripts/server.sh
+++ kernel-tree-scripts/server.sh
@@ -146,7 +146,7 @@ function installQuestions() {
 		read -rp "WireGuard interface name: " -e -i wg0 SERVER_WG_NIC
 	done
 
-	until [[ ${SERVER_WG_IPV4} =~ ^([0-9]{1,3}\.){3} ]]; do
+	until [[ ${SERVER_WG_IPV4} =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; do
 		read -rp "Server WireGuard IPv4: " -e -i 192.168.96.1 SERVER_WG_IPV4
 	done
 
